0c659923a2cc2a92511e22046e0cd3b416db4812
Haider Ali Chaudhary <haideralichoudhary@gmail.com>
2024-10-01 15:08:34 +0300

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private ResponseDTO _response;
+        private readonly AppDBContext _db;
+        private IMapper _mapper;
+
+        public CouponAPIController(AppDBContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDTO();
+        }
+
+        [HttpGet]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objlist = _db.Coupons.ToList();
+                _response.Result = _mapper.Map< IEnumerable<CouponDto>>(objlist);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Coupon objlist = _db.Coupons.First(x => x.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(objlist);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Specify Identity column
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        [Required]
+        public int MinAmount { get; set; }  
+        public DateTime? LastUpdated { get; set; }
+    }
+}
